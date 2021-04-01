13cdf4c862d293d4d96536734b28fbe3a968f453
Ana Potoroaca <53191102+Ana-poto@users.noreply.github.com>
2021-04-01 14:39:17 +0300

    some testing methods

--- BlazorServer/Models/Entities/User.cs
+++ BlazorServer/Models/Entities/User.cs
@@ -5,6 +5,14 @@ namespace WebApplication1.Models
 {
     public class User : BaseEntity
     {
+        public User(){}
+
+        public User(string v1, string v2)
+        {
+            this.Email = v1;
+            this.Password = v2;
+        }
+
         [Required]
         public string Email { get; set; }
 
