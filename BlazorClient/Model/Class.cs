7809175987388356f682023f31280cc8f228eaad
FluffyPanda69 <lemonsarecubic@gmail.com>
2021-04-05 12:38:15 +0300

    Clientside validation for Login/Register forms

--- BlazorClient/Model/Class.cs
+++ BlazorClient/Model/Class.cs
@@ -5,8 +5,26 @@ namespace BlazorClient.Model
     public class LoginModel
     {
         [Required]
+        [RegularExpression(@"^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$", ErrorMessage = "Username does not appear to be a valid email adress")]
         public string username { get; set; }
+
         [Required]
+        [RegularExpression(@"^(?=.*[a-z])(?=.*[A-Z])(?=.*\d).{8,15}$", ErrorMessage = "Password must contain at least 8 characters, one capital letter and one number")]
         public string password { get; set; }
+    }
+
+    public class RegisterModel
+    {
+        [Required]
+        [RegularExpression(@"^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$", ErrorMessage = "Username does not appear to be a valid email adress")]
+        public string username { get; set; }
+
+        [Required]
+        [RegularExpression(@"^(?=.*[a-z])(?=.*[A-Z])(?=.*\d).{8,15}$", ErrorMessage = "Password must contain at least 8 characters, one capital letter and one number")]
+        public string password1 { get; set; }
+
+        [Required]
+        [CompareProperty("password1", ErrorMessage = "Passwords must match !")]
+        public string password2 { get; set; }
     }
 }
