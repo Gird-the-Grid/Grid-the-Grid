f93158c5cc8f38d668ebeb40eb6769e2827dc879
unknown <engineeredbase484@gmail.com>
2021-04-15 12:27:24 +0300

    grid validation & text

--- BlazorClient/Model/Entities/GridModelTemplate.cs
+++ BlazorClient/Model/Entities/GridModelTemplate.cs
@@ -1,12 +1,15 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using System;
+using System.ComponentModel.DataAnnotations;
 
 namespace BlazorClient.Model.Entities
 {
     public class GridModelTemplate : BaseEntity
     {
+        [Range(2, 2000000)]
         [Required]
         public int Vertexes { get; set; }
 
+        //[Range(0, _Vertexes * (_Vertexes - 1) / 4)] // TODO:Rezolvare eroare CS0120
         [Required]
         public int Edges { get; set; }
         public string OwnerId { get; set; }
