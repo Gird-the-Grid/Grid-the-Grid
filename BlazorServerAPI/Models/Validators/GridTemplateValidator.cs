7bd4b8c88f5906de129e1b58577cd1fac62fbb97
ancestor-mithril <sgeorge.sstoica99@gmail.com>
2021-04-22 13:38:08 +0300

    Changed graph validator response

--- BlazorServerAPI/Models/Validators/GridTemplateValidator.cs
+++ BlazorServerAPI/Models/Validators/GridTemplateValidator.cs
@@ -10,7 +10,7 @@ namespace BlazorServerAPI.Models.Validators
             RuleFor(gridTemplate => gridTemplate.Vertexes).InclusiveBetween(2, 1900000).WithMessage("Clients on greed must be between 2 and 1900000");
             RuleFor(gridTemplate => gridTemplate.Edges).GreaterThan(1).Must((gridTemplate, edges) => {
                 return gridTemplate.Vertexes * (gridTemplate.Vertexes - 1) / 4 > edges;
-            }).WithMessage(gridTemplate => $"Conections on greed must be between 1 and {gridTemplate.Vertexes * (gridTemplate.Vertexes - 1) / 4}");
+            }).WithMessage(gridTemplate => $"Conections on greed must be between 1 and {gridTemplate.Vertexes * (gridTemplate.Vertexes - 1) / 4 - 1}");
         }
     }
 }
