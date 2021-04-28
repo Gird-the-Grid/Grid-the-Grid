bd9fdbc32c295258762669806f05ecf68262ef3e
Ana-poto <a_potoroaca@yahoo.com>
2021-04-28 11:15:42 +0300

    #2 Unit testing and integration test
    *deleted BlazorServerTests (I will try to add an alternative version to them)

---
+++ IntegrationTests/IndexPageTests.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc.Testing;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace IntegrationTests
+{
+    public class IndexPageTests : IClassFixture<CustomWebApplicationFactory<BlazorServerAPI.Startup>>
+    {
+        private readonly HttpClient _client;
+        private readonly CustomWebApplicationFactory<BlazorServerAPI.Startup> _factory;
+
+        public IndexPageTests(
+            CustomWebApplicationFactory<BlazorServerAPI.Startup> factory)
+        {
+            _factory = factory;
+            _client = factory.CreateClient(new WebApplicationFactoryClientOptions
+            {
+                AllowAutoRedirect = false
+            });
+        }
+    }
+}
