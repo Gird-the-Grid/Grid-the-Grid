7f874ee9bb3161e35eb9cdd750f437b38aeb3bc7
ancestor-mithril <sgeorge.sstoica99@gmail.com>
2021-05-25 20:50:58 +0300

    Solved all remaindered frontend logic

--- BlazorClient/Model/NavMenuService.cs
+++ BlazorClient/Model/NavMenuService.cs
@@ -8,9 +8,15 @@ namespace BlazorClient
     {
         private List<NavMenu> AdditionalMenuItems { get; set; }
         public event EventHandler<EventArgs> OnChanged;
+        public event EventHandler<EventArgs> OnExit;
         public void NotifyChanged()
         {
             OnChanged.Invoke(this, EventArgs.Empty);
+        }
+
+        public void StopTimer()
+        {
+            OnExit.Invoke(this, EventArgs.Empty);
         }
     }
 }
